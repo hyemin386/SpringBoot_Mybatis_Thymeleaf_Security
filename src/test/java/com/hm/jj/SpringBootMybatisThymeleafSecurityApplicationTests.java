3118562834da06baf91e04ca651fdfd92ad2ea86
hyemin386 <ha_ha24@naver.com>
2021-05-24 09:43:35 +0900

    Initial commit

---
+++ src/test/java/com/hm/jj/SpringBootMybatisThymeleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.jj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisThymeleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
